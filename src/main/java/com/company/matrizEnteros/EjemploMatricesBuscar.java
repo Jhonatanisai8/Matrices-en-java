74d7c478d296594c25b8da709ff092f6bdc893fd
jho-my <ojedasnachezjhonatan@gmail.com>
2024-04-15 12:26:28 -0500

    Actualizacion del metodo del buscar

--- src/main/java/com/company/matrizEnteros/EjemploMatricesBuscar.java
+++ src/main/java/com/company/matrizEnteros/EjemploMatricesBuscar.java
@@ -13,20 +13,24 @@ public class EjemploMatricesBuscar {
     public static void ejemplo() {
         int matriz[][] = {
             {35, 67, 78, 244},
-            {12, 234, 55, 76},
+            {12, 234, 123, 76},
             {11, 66, 55, 88}
         };
 
-        int buscar = 9999;
+        System.out.println("Nº de filas: " + matriz.length);
+        System.out.println("Nº de columnas: " + matriz[0].length);
+        int buscar = 55;
         boolean encontrado = false;
         buscar(matriz, buscar, encontrado);
-        
+
     }
 
     public static void buscar(int matriz[][], int eleBuscar, boolean estado) {
+        int i;
+        int j = 0;
         buscar:
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
+        for (i = 0; i < matriz.length; i++) {
+            for (j = 0; j < matriz[i].length; j++) {
                 if (matriz[i][j] == eleBuscar) {
                     estado = true;
                     break buscar;
@@ -35,9 +39,9 @@ public class EjemploMatricesBuscar {
         }
 
         if (estado) {
-            System.out.println("Encontrado");
+            System.out.println("Elemento " + eleBuscar + " Encontrado en la posicion : " + i + " " + j);
         } else {
-            System.out.println("NO encontrado");
+            System.out.println("Elemento " + eleBuscar + "no encontrado");
         }
     }
 }
