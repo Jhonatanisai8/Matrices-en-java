373c78edd8363c635cd8254cfe09effdf9393124
jho-my <ojedasnachezjhonatan@gmail.com>
2024-04-15 22:06:09 -0500

    Metodo para marcar la diagonal Superior con 1

---
+++ src/main/java/com/company/matrizEnteros/DiagonalSupierior.java
@@ -0,0 +1,39 @@
+package com.company.matrizEnteros;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class DiagonalSupierior {
+
+    public static void main(String[] args) {
+        ejemplo();
+    }
+
+    public static void ejemplo() {
+        int matriz[][] = new int[5][5];
+        rellenarMatriz(matriz);
+        
+    }
+
+    public static void rellenarMatriz(int matriz[][]) {
+        System.out.println("Rellenando la Matriz con ceros y unos ");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (i == j) {
+                    matriz[i][j] = 1;
+                } 
+            }
+        }
+        mostrarMatriz(matriz);
+    }
+
+    public static void mostrarMatriz(int matriz[][]) {
+        for (int[] matriz1 : matriz) {
+            for (int j = 0; j < matriz1.length; j++) {
+                System.out.print("[" + matriz1[j] + "]\t");
+            }
+            System.out.println("");
+        }
+    }
+}
